109bc41df443f257d6acb8d3f8fab9f85cff8a7b
Bruna Machado <fmachadobruna@gmail.com>
2022-07-21 18:32:18 -0300

    Desafio Funcionando

--- desafio/forca.js
+++ desafio/forca.js
@@ -1,15 +1,58 @@
 class Forca {
+  
+  constructor(palavraSecreta) {
+    this.palavraSecreta = palavraSecreta;
+    this.resultado = "aguardando chute";
+    this.letrasChutadas=[]
+    this.vidas = 6
+    this.palavra=[]
+    for (let i of this.palavraSecreta) {this.palavra.push("_")}
+  }
 
-  chutar(letra) { }
+    // array(palavraSecreta.length).fill('_') comentei pq da outra forma fica melhor
+    //chutar que recebe o parametro letra, quero armazenar as letras chutadas, sempre q teclar uma letra a função chutar deve ser chamada
 
-  buscarEstado() { return ""; } // Possiveis valores: "perdeu", "aguardando chute" ou "ganhou"
-
-  buscarDadosDoJogo() {
-      return {
-          letrasChutadas: [], // Deve conter todas as letras chutadas
-          vidas: 6, // Quantidade de vidas restantes
-          palavra: [] // Deve ser um array com as letras que já foram acertadas ou o valor "_" para as letras não identificadas
+  chutar(letra) { 
+    if(letra.length === 1 && this.palavraSecreta.includes(letra) == false && this.letrasChutadas.includes(letra) == false){
+      this.letrasChutadas.push(letra);
+      this.vidas--;
+    }
+    
+    if(letra.length === 1 && this.palavraSecreta.includes(letra) == true && this.letrasChutadas.includes(letra) == false){
+      this.letrasChutadas.push(letra);
+      for (let i = 0; i < this.palavraSecreta.length; i++) {
+        if (this.palavraSecreta[i] == letra){
+            this.palavra[i] = letra;
+        }
       }
+    }
+  }
+  
+  buscarEstado() { 
+    if (this.vidas === 0) {
+      return (this.resultado = "perdeu"); 
+    }
+    
+    else if (this.palavra.join("") === this.palavraSecreta && this.vidas > 0) {
+      return (this.resultado = "ganhou");
+    }
+    
+    let result = this.resultado;
+    
+    return result; 
+  }
+  // Possiveis valores: "perdeu", "aguardando chute" ou "ganhou"
+  
+  buscarDadosDoJogo() {
+    let letrasChutadas = this.letrasChutadas;
+    let vidas = this.vidas;
+    let palavra = this.palavra;
+    
+    return {
+      letrasChutadas, // Deve conter todas as letras chutadas
+      vidas, // Quantidade de vidas restantes
+      palavra // Deve ser um array com as letras que já foram acertadas ou o valor "_" para as letras não identificadas
+    }
   }
 }
 
